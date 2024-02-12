34c56e8212c45e9dc85d3c0fdc19a17419ddb8e8
Damir Kadyrzhanov <damirk120404@gmail.com>
2024-02-12 21:26:30 +0600

    words repetition task is completed

--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,21 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        String[] words = sentence.split("[, ]");
+        Map<String, Integer> result = new HashMap<>();
+        for (String word : words) {
+            int counter = 0;
+            for (int j = 1; j < words.length; j++) {
+                if (word.equals(words[j])) {
+                    counter++;
+                }
+            }
+            result.put(word, counter);
+        }
+        return result;
     }
 }
