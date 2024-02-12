2d21e3fc4cdaa95ca1e75b7d7fed8bdfd33fe5b5
Damir Kadyrzhanov <damirk120404@gmail.com>
2024-02-12 22:03:03 +0600

    words repetition works correctly

--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -2,19 +2,20 @@ package com.epam.mjc.collections.map;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.StringTokenizer;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
-        String[] words = sentence.split("[, ]");
+        String[] words = sentence.split("\\P{L}+");
         Map<String, Integer> result = new HashMap<>();
         for (String word : words) {
             int counter = 0;
-            for (int j = 1; j < words.length; j++) {
-                if (word.equals(words[j])) {
+            for (String s : words) {
+                if (word.equalsIgnoreCase(s)) {
                     counter++;
                 }
             }
-            result.put(word, counter);
+            result.put(word.toLowerCase(), counter);
         }
         return result;
     }
