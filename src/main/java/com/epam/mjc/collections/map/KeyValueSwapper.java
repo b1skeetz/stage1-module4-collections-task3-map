ba95ab1db72b09b7d5ae3e9a4672da74fcace8b0
Damir Kadyrzhanov <damirk120404@gmail.com>
2024-02-13 21:28:04 +0600

    keyvaluetask is done

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,8 +1,34 @@
 package com.epam.mjc.collections.map;
 
-import java.util.Map;
+import java.util.*;
 
 public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+        Map<String, Integer> result = new HashMap<>();
+        List<String> buffer = new ArrayList<>();
+        for (Map.Entry<Integer, String> e : sourceMap.entrySet()) {
+            buffer.add(e.getKey().toString());
+            buffer.add(e.getValue());
+        }
+        for (int i = 1; i < buffer.size(); i += 2) {
+            for (int j = i; j < buffer.size(); j += 2) {
+                if (buffer.get(i).equals(buffer.get(j))) {
+                    int num1 = Integer.parseInt(buffer.get(i - 1));
+                    int num2 = Integer.parseInt(buffer.get(j - 1));
+                    if (num1 > num2) {
+                        buffer.remove(i);
+                        buffer.remove(i - 1);
+                    } else if (num2 > num1) {
+                        buffer.remove(j);
+                        buffer.remove(j - 1);
+                    }
+                }
+            }
+        }
+        for(int i = 1; i < buffer.size(); i+=2){
+            result.put(buffer.get(i), Integer.parseInt(buffer.get(i-1)));
+        }
+
+        return result;
     }
 }
